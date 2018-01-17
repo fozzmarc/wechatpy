b8974d17f7e928f08739389f0776c0e5aa4e50a3
fozzz <fozzmarc@gmail.com>
2018-01-18 07:20:04 +0800

    Update base.py
    verify false

--- wechatpy/client/api/base.py
+++ wechatpy/client/api/base.py
@@ -10,6 +10,7 @@ class BaseWeChatAPI(object):
     def _get(self, url, **kwargs):
         if getattr(self, 'API_BASE_URL', None):
             kwargs['api_base_url'] = self.API_BASE_URL
+        kargs['verify'] = False
         return self._client.get(url, **kwargs)
 
     def _post(self, url, **kwargs):
